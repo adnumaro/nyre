ee35a65d2d29aa42e9f7398ce3beb0070bb4ea48
adrian <adrian@yumiwi.com>
2019-12-28 13:38:16 +0100

    Update regex validation of color type

--- src/survey/patterns/composite/types/primitives/ColorType.ts
+++ src/survey/patterns/composite/types/primitives/ColorType.ts
@@ -3,27 +3,29 @@ import * as t from 'io-ts'
 
 // https://stackoverflow.com/questions/43706082/validation-hex-and-rgba-colors-using-regex-in-php
 // https://regex101.com/r/A2IjNO/4
-const HEX = '\#[\da-f]{3}|\#[\da-f]{6}'
-const RGB = 'rgb\(' +
-            '((\d{1,2}|1\d\d|2([0-4]\d|5[0-5]))\s*,\s*){2}' +
-            '((\d{1,2}|1\d\d|2([0-4]\d|5[0-5]))\s*)' +
-            '\)'
-const RGBA = 'rgba\(' +
-             '((\d{1,2}|1\d\d|2([0-4]\d|5[0-5]))\s*,\s*){2}' +
-             '((\d{1,2}|1\d\d|2([0-4]\d|5[0-5]))\s*)' +
-             '(,\s*(0\.\d+|1))' +
-             '\)'
-const HSL = 'hsl\(' +
-            '\s*((\d{1,2}|[1-2]\d{2}|3([0-5]\d|60)))\s*,' +
-            '\s*((\d{1,2}|100)\s*%)\s*,' +
-            '\s*((\d{1,2}|100)\s*%)' +
-            '\)'
-const HSLA = 'hsla\(' +
-             '\s*((\d{1,2}|[1-2]\d{2}|3([0-5]\d|60)))\s*,' +
-             '\s*((\d{1,2}|100)\s*%)\s*,' +
-             '\s*((\d{1,2}|100)\s*%)' +
-             '(,\s*(0\.\d+|1))' +
-             '\)'
+const HEX = '\\#[\\da-f]{3}|\\#[\\da-f]{6}'
+const RGB = 'rgb\\(\\s*' +
+            '(\\d{1,2}|1\\d\\d|2([0-4]\\d|5[0-5]))\\s*,' +
+            '\\s*(\\d{1,2}|1\\d\\d|2([0-4]\\d|5[0-5]))\\s*,' +
+            '\\s*(\\d{1,2}|1\\d\\d|2([0-4]\\d|5[0-5]))' +
+            '\\s*\\)'
+const RGBA = 'rgba\\(\\s*' +
+             '(\\d{1,2}|1\\d\\d|2([0-4]\\d|5[0-5]))\\s*,' +
+             '\\s*(\\d{1,2}|1\\d\\d|2([0-4]\\d|5[0-5]))\\s*,' +
+             '\\s*(\\d{1,2}|1\\d\\d|2([0-4]\\d|5[0-5]))\\s*,' +
+             '\\s*(0?\\.\\d+|1)' +
+             '\\s*\\)'
+const HSL = 'hsl\\(\\s*' +
+            '((\\d{1,2}|[1-2]\\d{2}|3([0-5]\\d|60)))\\s*,' +
+            '\\s*((\\d{1,2}|100)\\s*%)\\s*,' +
+            '\\s*((\\d{1,2}|100)\\s*%)' +
+            '\\s*\\)'
+const HSLA = 'hsla\\(\\s*' +
+             '((\\d{1,2}|[1-2]\\d{2}|3([0-5]\\d|60)))\\s*,' +
+             '\\s*((\\d{1,2}|100)\\s*%)\\s*,' +
+             '\\s*((\\d{1,2}|100)\\s*%)\\s*' +
+             '(,\\s*(0?\\.\\d+|1))' +
+             '\\s*\\)'
 
 const PATTERN = new RegExp(`^(${HEX}|${RGB}|${RGBA}|${HSL}|${HSLA})$`, 'i')
 
@@ -34,7 +36,7 @@ export default function URLType (type: string) {
     (input: any, context: any) => (
       typeof input === 'string' && (PATTERN.test(input) || !input)
         ? t.success(input)
-        : t.failure(input, context, `Type error: ${type} has invalid url`)
+        : t.failure(input, context, `Type error: ${type} has invalid representation => ${input}`)
     ),
     t.identity
   )
